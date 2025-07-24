8f0c6d980c32d6aa4ce782892de6903ffcf0b8c1
dzungofTheShadow <23127003@student.hcmus.edu.vn>
2025-07-24 22:08:48 +0700

    add notes comments for inference

--- source/inference.py
+++ source/inference.py
@@ -1,2 +1,8 @@
+'''
+Class InferenceEngine:
+- Add new facts to agent's knowledge base
+- Infer if a given position is safe/unsafe
+- Update knowledge base after wumpuses move
+'''
 class InferenceEngine:
     pass
