45620cb5f2a42b7376f891d7aa73f53ce44fe1ec
dzungofTheShadow <23127003@student.hcmus.edu.vn>
2025-07-24 22:11:27 +0700

    edit notes

--- source/inference.py
+++ source/inference.py
@@ -3,6 +3,7 @@ Class InferenceEngine:
 - Add new facts to agent's knowledge base
 - Infer if a given position is safe/unsafe
 - Update knowledge base after wumpuses move
+- Agent has an instance of this class
 '''
 class InferenceEngine:
     pass
--- source/planning.py
+++ source/planning.py
@@ -1,2 +1,8 @@
+'''
+Class Planner:
+- Decide action for agent after inference
+- Implement search algo like A* to choose the action
+- Agent has an instance of this class
+'''
 class Planner:
     pass
