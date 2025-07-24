118f3e3a5c16d3f2041c96f0a189338d48b713da
dzungofTheShadow <23127003@student.hcmus.edu.vn>
2025-07-24 21:56:55 +0700

    add notes comments for environment file

--- source/environment.py
+++ source/environment.py
@@ -1,2 +1,9 @@
+'''
+The Environment class contains:
+- Agent
+- Information on locations of everything
+- Get percept and give it to the agent
+- Render everything after each loop
+'''
 class Environment:
     pass
