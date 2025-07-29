7defdb9713cb5c329842c1c4ca411f0fd2accf7b
dzungofTheShadow <23127003@student.hcmus.edu.vn>
2025-07-29 18:46:31 +0700

    remove old files

--- source/environment.py
+++
@@ -1,9 +0,0 @@
-'''
-The Environment class contains:
-- Agent
-- Information on locations of everything
-- Get percept and give it to the agent
-- Render agent's knowledge after each loop
-'''
-class Environment:
-    pass
--- source/inference.py
+++
@@ -1,9 +0,0 @@
-'''
-Class InferenceEngine:
-- Add new facts to agent's knowledge base
-- Infer if a given position is safe/unsafe
-- Update knowledge base after wumpuses move
-- Agent has an instance of this class
-'''
-class InferenceEngine:
-    pass
