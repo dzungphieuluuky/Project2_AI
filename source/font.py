aa873bd96eca2a048f71752c8a7ad3b4e4337560
dzungofTheShadow <23127003@student.hcmus.edu.vn>
2025-08-02 23:09:51 +0700

    remove gui files from project 1

--- source/font.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-pygame.init()
-font = pygame.font.SysFont("Roboto", 96, bold=True)
-button_font = pygame.font.SysFont("Cascadia Mono", 32)
-title_font = pygame.font.SysFont("Consolas", 60, bold=True)
-body_font = pygame.font.SysFont("Consolas", 28)
-intro_font = pygame.font.SysFont("Consolas", 20)
--- source/sound.py
+++
@@ -1,5 +0,0 @@
-import pygame
-
-pygame.init()
-hover_sound = pygame.mixer.Sound('./assets/click.mp3')
-click_sound = pygame.mixer.Sound('./assets/mouse-click.mp3')
